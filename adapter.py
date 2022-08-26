b77090da7b42f19d78c5a45df93dde052271bcfe
Marco Germanier <marco@mrgt.me>
2022-08-26 22:46:32 +0100

    Update adapter.py
    update with new api, comment outuseless params

--- adapter.py
+++ adapter.py
@@ -1,14 +1,14 @@
 from bridge import Bridge
 import os
 
+print('og')
 class Adapter:
 #    base_url = "https://api.twitter.com/2/users/1018093644/tweets?max_results=5"
 #    from_params = ['base', 'from', 'coin']
 #    to_params = ['quote', 'to', 'market']
 
     def __init__(self, input):
-        self.id = input.get('id', '1')
-
+#        self.id = input.get('id', '1')
         self.request_data = input.get('data')
         if self.validate_request_data():
             self.bridge = Bridge()
@@ -28,7 +28,7 @@ class Adapter:
 
     def set_params(self):
         self.twitter_id = int(self.request_data.get("twitter_id"))
-        self.address_owner = self.request_data.get("address_owner")
+        print(self.twitter_id)
         # for param in self.from_params:
         #     self.from_param = self.request_data.get(param)
         #     if self.from_param is not None:
@@ -48,30 +48,15 @@ class Adapter:
                 "Authorization": "Bearer " + os.environ.get('BEARER_TOKEN')
                 }
             
-            print('pepe')
             # twitter api
-            base_url = 'https://api.twitter.com/2/users/{}/tweets?max_results=5'.format(params['id']) 
-            print('pepe')
-            
+            base_url="https://api.twitter.com/2/users?ids={}&user.fields=id,name,profile_image_url,username".format(params['id']) 
+           
             response = self.bridge.request(base_url, headers=headers)
 
             # response from external api
-            data = response.json()
-            # parse response data
-            address_owner = data["data"][0]["text"].split(' ')[5]
-            if (address_owner.strip().lower() == self.address_owner.strip().lower()):
-
-                # bitwise operator to fit twitter_id + address in same res
-                twitter_id_res =  int(self.twitter_id)<<160
-
-                #address coming from twitter
-                address_res = int(address_owner,16)
-
-                self.result=address_res+twitter_id_res
-            else:
-                self.result = 0
-
+            self.result = response.json()
             self.result_success(self.result)
+
         except Exception as e:
             self.result_error(e)
         finally:
@@ -79,15 +64,15 @@ class Adapter:
 
     def result_success(self, data):
         self.result = {
-            'jobRunID': self.id,
-            'data': data,
+#            'jobRunID': self.id,
+#            'data': data,
             'result': self.result,
             'statusCode': 200,
         }
 
     def result_error(self, error):
         self.result = {
-            'jobRunID': self.id,
+#            'jobRunID': self.id,
             'status': 'errored',
             'error': f'There was an error: {error}',
             'statusCode': 500,
